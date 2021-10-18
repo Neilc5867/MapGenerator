36beda5bcafca530a90d376fd4e302493b4c5abc
neilc5867 <neilc5867@gmail.com>
2021-10-18 12:36:00 +0100

    Initial commit - MapGenerator.py created.

---
+++ MapGenerator.py
@@ -0,0 +1,14 @@
+import random
+import array
+import numpy.matlib
+import matplotlib
+import math
+
+"""
+" MapGenerator
+"
+" A file that creates a visual map generated from random values, containing
+" geographical features for a fantasy setting.
+"
+" @Author Neil Cronin neilc5867@gmail.com
+"""
